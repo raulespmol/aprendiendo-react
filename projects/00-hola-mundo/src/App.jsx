d8bf015fe648d1913c17cbba3caed36db67b7040
Raul Espinoza M <raul.espmol@gmail.com>
2023-10-07 23:10:58 -0300

    hover botones

--- projects/00-hola-mundo/src/App.jsx
+++ projects/00-hola-mundo/src/App.jsx
@@ -5,9 +5,8 @@ import { FollowCard } from './FollowCard.jsx'
 export function App () {
   return (
     <div className='card'>
-      <FollowCard userName='Raúl Espinoza M' user='raulespmol' isFollowing={true} />
-      <FollowCard userName='Elon Musk' user='elonmusk' isFollowing />
-      <FollowCard userName='Nino Moonless' user='chayobee' isFollowing={false} />
+      <FollowCard userName='Raúl Espinoza M' user='raulespmol' initialIsFollowing={true} />
+      <FollowCard userName='Nino Moonless' user='chayobee' initialIsFollowing={false} />
     </div>
   )
 }
--- projects/00-hola-mundo/src/FollowCard.jsx
+++ projects/00-hola-mundo/src/FollowCard.jsx
@@ -1,16 +1,24 @@
-// import './App.css'
+import { useState } from 'react';
+import './App.css';
 
-export function FollowCard ( {userName, user, isFollowing} ) {
-const avatarSource = `https://unavatar.io/${user}`;
-const buttonText = isFollowing ? 'Siguiendo' : 'Seguir'
-const buttonClassName = isFollowing ?
-  'follow-button isFollowing' :
-  'follow-button';
+export function FollowCard ( {userName, user, initialIsFollowing} ) {
+
+  const [isFollowing, setIsFollowing] = useState(initialIsFollowing);
+
+  const avatarSource = `https://unavatar.io/${user}`;
+  const buttonText = isFollowing ? 'Siguiendo' : 'Seguir'
+  const buttonClassName = isFollowing ?
+    'follow-button isFollowing-button' :
+    'follow-button';
+
+  const toggleButton = () => {
+    setIsFollowing(!isFollowing)
+  }
 
   return (
     <section className="follow-card">
       <header className="follow-card-header">
-        <img src={avatarSource} alt="Avatar" className="follow-card-avatar"/>
+        <img src={avatarSource} alt={`Foto de perfil de ${userName}`} className="follow-card-avatar"/>
         <div className="follow-card-info">
           <strong className="user-name">{userName}</strong>
           <span className="user">@{user}</span>
@@ -18,8 +26,11 @@ const buttonClassName = isFollowing ?
       </header>
 
       <aside>
-        <button className={buttonClassName}>
-          {buttonText}
+        <button 
+        className={buttonClassName}
+        onClick={toggleButton}>
+          <span className='button-text'>{buttonText}</span>
+          <span className='button-unfollow'>Dejar de seguir</span>
         </button>
       </aside>
     </section>
